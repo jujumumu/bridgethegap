e8705aa09a4e0a366f764db808db86afac6857f3
Justin Mu <52044956+jujumumu@users.noreply.github.com>
2022-10-30 08:16:26 -0700

    asdsad

--- Wall.gd
+++ Wall.gd
@@ -12,9 +12,10 @@ func move():
 
 func _on_Tween_tween_all_completed():
 	count = count + 1
-	get_node("StaticBody2D").get_node(str(count)).visible = false
-	if count == 9:
+	
+	if count >= 9:
 		get_node("StaticBody2D/CollisionShape2D").disabled = true
 		return;
+	get_node("StaticBody2D").get_node(str(count)).visible = false
 	get_node("Tween").interpolate_property(get_node("StaticBody2D"), "position", Vector2(0,count*30), Vector2(0,30*count+30), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	get_node("Tween").start()
